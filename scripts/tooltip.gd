767e084b0a109c63842420e933158d4c7798048f
Rosan <rosan.syal@gmail.com>
2026-07-16 17:55:27 -0400

    created pop up tooltip

--- scripts/tooltip.gd
+++ scripts/tooltip.gd
@@ -3,6 +3,8 @@ extends PanelContainer
 const OFFSET: Vector2 = Vector2.LEFT * 250.0
 @onready var label: RichTextLabel = $RichTextLabel
 
+var animation_tween: Tween
+
 func _ready() -> void:
 	hide()
 
@@ -13,7 +15,17 @@ func _input(event: InputEvent) -> void:
 func toggle(on: bool):
 	if on:
 		show()
+		modulate.a = 0.0
+		if animation_tween:
+			animation_tween.kill()
+		animation_tween = create_tween().set_trans(Tween.TRANS_SINE)
+		animation_tween.tween_property(self, "modulate:a", 1.0, 0.3)
 	else:
+		modulate.a = 1.0
+		if animation_tween:
+			animation_tween.kill()
+		animation_tween = create_tween().set_trans(Tween.TRANS_SINE)
+		await animation_tween.tween_property(self, "modulate:a", 0.0, 0.1).finished
 		hide()
 	
 func setLabel(name: String, desc: String, cost: int):
