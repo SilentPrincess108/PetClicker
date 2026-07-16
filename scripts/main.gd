fd449e07a94f8d52a00eeb20c469b82d043ac47f
Rosan <rosan.syal@gmail.com>
2026-07-16 19:01:29 -0400

    custom mouse cursors

--- scripts/main.gd
+++ scripts/main.gd
@@ -3,11 +3,11 @@ extends Control
 @onready var num_clicks: Label = $mainPanel/numClicks
 @onready var pet: PetButton = $mainPanel/CenterContainer/PetButton
 
-var arrow_cursor = load("res://assets/sprites/cursortest.png")
+var arrow_cursor = load("res://assets/sprites/default.png")
 
 func _ready() -> void:
 	print("game start")
-	#Input.set_custom_mouse_cursor(arrow_cursor, Input.CURSOR_ARROW, Vector2(50, 50))
+	Input.set_custom_mouse_cursor(arrow_cursor, Input.CURSOR_ARROW, Vector2(16, 16))
 	
 func _process(_delta: float) -> void:
 	num_clicks.text = "Clicks: " + str(Globals.clicks)
